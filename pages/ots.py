fc55a051eec8bace6d0cbbe458fc0ee96ff1be91
DanaDririon <daniel.rs0804@gmail.com>
2025-04-23 18:23:29 -0400

    ots container filtros

--- pages/ots.py
+++ pages/ots.py
@@ -7,15 +7,19 @@ from time import sleep
 import os
 
 
-def filtros_detalles(df, rut=None, cod_nubox=None, nombre=None, fecha=None, patente=None, estado=None) -> pd.DataFrame:
-    if rut != None:
-        df = df[df['RUT Cliente']==rut]
+def filtros_detalles(df, rut=None, cod_nubox=None, nombre=None, fecha=None, patente=None, estado=None, rut_name=None) -> pd.DataFrame:
+    if rut_name != None:
+        #df = df[df['RUT Cliente']==rut]
+        df = df[df['rut_name']==rut_name]
     if patente != None:
         df = df[df['Patente']==patente]
     if nombre != None:
         df = df[df['Nombre Cliente']==nombre]
     if estado != None:
-        df = df[df['Estado']==estado]
+        if estado=="Finalizadas":
+            df = df[df['Estado']=="Finalizada"]
+        else:
+            df = df[df['Estado']!="Finalizada"]
     if fecha != None:
         if len(fecha) == 2:
             df = df[(pd.to_datetime(df['Fecha'])>=pd.to_datetime(fecha[0])) & (pd.to_datetime(df['Fecha'])<=pd.to_datetime(fecha[1]))]
@@ -67,6 +71,8 @@ def main():
     st.markdown("<h1>"+"Órdenes de Trabajo"+"</h1>", unsafe_allow_html=True)
     sidebar()
 
+
+
     df_ots = pd.DataFrame({
         "Id Orden": [11111, 22222, 33333, 44444, 55555, 66666, 77777],
         "RUT Cliente": ["19.334.061-4","19.334.061-4","19.334.061-4","7.930.134-5","18.887.218-K","6.622.912-0","15.898.932-9"],
@@ -78,42 +84,81 @@ def main():
         #VIN
         #Descripcion
         #"Tipo Trabajo"
-        "Estado":["Finalizado", "Desarme", "Armado", "Para entrega", "Proc. de repuesto","En preparación","Facturado"],
+        "Estado":["Finalizada", "Desarme", "Armado", "Para entrega", "Proc. de repuesto","En preparación","Facturada"],
         "Fecha Ingreso":["03-MAR-2025", "05-ABR-2025", "05-ABR-2025", "20-MAR-2025", "28-MAR-2025","02-ABR-2025","23-ABR-2025"],
         "Fecha Entrega":["20-MAR-2025","-","-","-","-","-","22-ABR-2025"]
     })
 
     df_detalle = pd.DataFrame
-
-    col1, col2 , col3, col4 = st.columns((0.5,0.5,0.5,0.5))
-
-    rut_filter = col1.selectbox("Buscar RUT", df_ots['RUT Cliente'].unique() , index=None, placeholder='RUT')
-    if rut_filter:
-        df_ots = filtros_detalles(df_ots, rut=rut_filter)
-
-    nombre_filter = col2.selectbox("Buscar Nombre", df_ots['Nombre Cliente'].unique() , index=None, placeholder='Nombre')
-    if nombre_filter:
-        df_ots = filtros_detalles(df_ots, nombre=nombre_filter)
     
-    patente_filter = col3.selectbox("Buscar Patente", df_ots['Patente'].unique() , index=None, placeholder='Patente')
-    if patente_filter:
-        df_ots = filtros_detalles(df_ots, patente=patente_filter)
 
-    estado_filter = col4.selectbox("Buscar Estado", df_ots['Estado'].unique() , index=None, placeholder='Estado')
-    if estado_filter:
-        df_ots = filtros_detalles(df_ots, estado=estado_filter)
+    a,b = st.columns((3.5,2))
+
+
+    with a.container(height=500):
+        df_ots['rut_name'] = df_ots['RUT Cliente'] +' | '+df_ots['Nombre Cliente']
+        col1, col2 , col3 , col4= st.columns((1,0.5,0.5,1))
+        #rut_filter = col1.selectbox("Buscar RUT", df_ots['RUT Cliente'].unique() , index=None, placeholder='RUT')
+        rut_filter = col1.selectbox("Buscar Cliente", df_ots['rut_name'].sort_values().unique() , index=None, placeholder='Cliente')
+        if rut_filter:
+            #df_ots = filtros_detalles(df_ots, rut=rut_filter)
+            df_ots = filtros_detalles(df_ots, rut_name=rut_filter)
+
+        #nombre_filter = col2.selectbox("Buscar Nombre", df_ots['Nombre Cliente'].unique() , index=None, placeholder='Nombre')
+        #if nombre_filter:
+            #df_ots = filtros_detalles(df_ots, nombre=nombre_filter)
+        
+        patente_filter = col2.selectbox("Buscar Patente", df_ots['Patente'].unique() , index=None, placeholder='Patente')
+        if patente_filter:
+            df_ots = filtros_detalles(df_ots, patente=patente_filter)
+
+        estado_filter = col3.selectbox("Buscar Estado", ("Abiertas","Finalizadas") , index=None, placeholder='Estado')
+        if estado_filter:
+            df_ots = filtros_detalles(df_ots, estado=estado_filter)
+
+        df_ots=df_ots[['Id Orden', 'RUT Cliente','Nombre Cliente', 'Patente', "Marca", "Estado"]]
+        data = st.dataframe(df_ots,
+                on_select='rerun',
+                selection_mode='single-row',
+                hide_index=True)
+
+    container_detalle = b.container(height=500)
+    container_detalle.subheader("Detalle OT")
+    if len(data.selection['rows']):
+        selected_row = data.selection['rows'][0]
+        st.session_state['selected_id_ot'] = df_ots.iloc[selected_row]['Id Orden']
+
+        #with b.container:
+        
+        detalle = container_detalle.table()
+        container_detalle.image(image=r'C:\slak.jpg', use_container_width =True)
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+        container_detalle.image(image=r'C:\slak.jpg')
+
+    else:
+        selected_row = None
+        st.session_state['selected_id_ot'] = None
+
     
-    df_ots
+
+    st.write(st.session_state)
 
     #left_co, cent_co,last_co = st.columns([0.5,1,0.5])
     cent_co = st.image("src\\img\\taller.png",use_container_width=True)
     #cent_co
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 #    import importlib
 #    importlib.reload(cs)
 
 #    cs.control_login(page,allow=True)
-
-main()
+    main()
