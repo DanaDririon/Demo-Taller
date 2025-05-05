4267aad38fe33854f764443b71a3d23cb0ccdb1b
DanaDririon <daniel.rs0804@gmail.com>
2025-05-05 17:11:46 -0400

    botones ots

--- pages/ots_nueva.py
+++ pages/ots_nueva.py
@@ -5,38 +5,39 @@ import numpy as np
 from time import sleep
 from control_taller import utils as ct
 import os
-import re
-
 
 def main():
+    
     #configuracion de pagina
-    st.set_page_config(layout="wide", page_title='Nuevo Cliente', page_icon="src\\img\\logo-servicena.png")
+    st.set_page_config(layout="wide", page_title='Nueva OT', page_icon="src\\img\\logo-servicena.png")
     ct.increase_page()
     ct.hide_deploy_button()
-    st.markdown("<h1>"+"Nuevo Cliente"+"</h1>", unsafe_allow_html=True)
+    st.markdown("<h1>"+"Nueva OT"+"</h1>", unsafe_allow_html=True)
     ct.sidebar()
 
     col1, col2, col3, col99 = st.columns((3,1,2,0.3))
 
     if col1.button(label="⬅Volver"):
-        st.switch_page("pages\\clientes.py")
+        st.switch_page("pages\\ots.py")
 
     with col1.container(height=570):
-        rut_1, rut_3, rut_2 = st.columns((9, 0.8, 1.8))
-        rut = None
-        dig_ver = None
-        x = rut_1.text_input("RUT", max_chars=8, placeholder="Ingresar RUT sin puntos ni dígito verificador")
-        check_rut = ct.check_int(x)
-        rut_clean = ""
-        if check_rut:
-            rut_clean = int(x)
-            dig_ver = ct.digito_verificador(x)
-            rut_clean = str(rut_clean)+'-'+str(dig_ver)
-        rut_2.text_input(label="Digito Verificador", disabled=True, value=dig_ver)
-        nombre = st.text_input("Nombre cliente", placeholder="Ingresar nombre de cliente").upper()
-        check_nombre = nombre.isupper()
-        correo = st.text_input("Correo Electronico", placeholder="Formato de correo es 'xxxx@xxxx.xx'")
-        check_correo = ct.validate_email_syntax(correo)
+        df_clientes = ct.select_data(tabla="clientes", columns='cliente_rut, cliente_nombre, cliente_correo, cliente_telefono, cliente_direccion', where="deleted = 0")
+        df_clientes_1 = df_clientes
+        df_clientes_1 = df_clientes_1.drop(columns=['cliente_rut'])        
+        df_clientes_1['rut_name'] = df_clientes_1['cliente_rut'] +' | '+df_clientes_1['cliente_nombre']
+
+        cliente, rut_3, rut_2 = st.columns((9, 0.8, 1.8))
+
+        rut_nombre = cliente.selectbox("Cliente", df_clientes_1['rut_name'], placeholder="Buscar Cliente")
+        patente = st.text_input("Nombre cliente", placeholder="Ingresar Patente").upper()
+        check_patente = patente.isupper()
+        marca = st.text_input("Marca", placeholder="Marca")
+        modelo = st.text_input("Modelo", placeholder="Modelo")
+        año = st.text_input("Año", max_chars=4, placeholder="Año")
+        check_año = ct.check_int(año)
+        vin = st.text_input("VIN", placeholder="VIN Vehículo")
+        descripcion = st.text_input("Descripción", placeholder="Descripción")
+
         telefono = st.text_input("Telefóno",max_chars=9,placeholder="Ingresar sólo los últimos 9 dígitos")
         check_telefono = ct.check_int(telefono)
         direccion = st.text_input("Dirección", placeholder="Ingresar dirección").upper()
@@ -47,7 +48,7 @@ def main():
         tel_ini = ""
         if check_telefono: tel_ini = "+56"
         resumen = pd.DataFrame({
-            "Preview": ["RUT","Nombre","Correo","Teléfono","Dirección"],
+            "Preview": ["RUT Nombre","Patente","Correo","Teléfono","Dirección"],
             " ": [rut_clean,nombre,correo,tel_ini+telefono,direccion],
         })
 
@@ -70,12 +71,10 @@ def main():
         else:
             agregar = st.button(label='Agregar',type="primary", disabled=True)
 
-
 if __name__ == "__main__":
+    
 #    import importlib
 #    importlib.reload(cs)
 
 #    cs.control_login(page,allow=True)
-
     main()
-
