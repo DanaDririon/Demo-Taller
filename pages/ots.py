317af6d70e580b7632c0b66a85b6c3e34a4445f1
sp0ts <eladio.15.ldp@gmail.com>
2025-05-12 15:45:20 -0400

    vista_img ot listo

--- pages/ots.py
+++ pages/ots.py
@@ -46,7 +46,6 @@ def imagenes(id_ots):
     df_img['img_dir'] = df_img['img_dir'].astype(str)
     #apply img_dir as path
     df_img['img_dir'] = df_img['img_dir'].apply(lambda x: str(Path("src") / "img" / "ot" / "OT-{}".format(id_ots) / x))
-    st.write(df_img)
     return df_img
 
 def cotizaciones(id_ots):
